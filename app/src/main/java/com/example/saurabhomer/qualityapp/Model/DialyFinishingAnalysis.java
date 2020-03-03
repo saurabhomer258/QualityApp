97bb5c4cd13adc9075aecbf6dea0b3837fab004e
Anjali Kumari <anjali.kumari@cura.tech>
2020-03-03 10:02:48 +0530

    DialysisFinishingAnalysis Model class added

---
+++ app/src/main/java/com/example/saurabhomer/qualityapp/Model/DialyFinishingAnalysis.java
@@ -0,0 +1,258 @@
+package com.example.saurabhomer.qualityapp.Model;
+
+import android.widget.Button;
+
+public class DialyFinishingAnalysis {
+    String mPrintingMRBO,mSlubs_Holes_NAR,mBrokenStitches,mSlipStitches,mSPI,mPukering,mLooseTensions,mSnapDefects,mNeedleMark,mOpenSeam,mPleats;
+    String mMissingStitches,mSkipRunOff,mIncorrectLabel,mWrongPlacement,mLooseNess,mCutDamage,mOthers,mStain,mOilMark,mStickers,mUncutThread,mOutOfSpec;
+    String mTotalDefect,mQualityOut,mProductionOut,mDamage,mDirty,mIron;
+    Button mNext,mDone;
+
+    public String getmPrintingMRBO() {
+        return mPrintingMRBO;
+    }
+
+    public void setmPrintingMRBO(String mPrintingMRBO) {
+        this.mPrintingMRBO = mPrintingMRBO;
+    }
+
+    public String getmSlubs_Holes_NAR() {
+        return mSlubs_Holes_NAR;
+    }
+
+    public void setmSlubs_Holes_NAR(String mSlubs_Holes_NAR) {
+        this.mSlubs_Holes_NAR = mSlubs_Holes_NAR;
+    }
+
+    public String getmBrokenStitches() {
+        return mBrokenStitches;
+    }
+
+    public void setmBrokenStitches(String mBrokenStitches) {
+        this.mBrokenStitches = mBrokenStitches;
+    }
+
+    public String getmSlipStitches() {
+        return mSlipStitches;
+    }
+
+    public void setmSlipStitches(String mSlipStitches) {
+        this.mSlipStitches = mSlipStitches;
+    }
+
+    public String getmSPI() {
+        return mSPI;
+    }
+
+    public void setmSPI(String mSPI) {
+        this.mSPI = mSPI;
+    }
+
+    public String getmPukering() {
+        return mPukering;
+    }
+
+    public void setmPukering(String mPukering) {
+        this.mPukering = mPukering;
+    }
+
+    public String getmLooseTensions() {
+        return mLooseTensions;
+    }
+
+    public void setmLooseTensions(String mLooseTensions) {
+        this.mLooseTensions = mLooseTensions;
+    }
+
+    public String getmSnapDefects() {
+        return mSnapDefects;
+    }
+
+    public void setmSnapDefects(String mSnapDefects) {
+        this.mSnapDefects = mSnapDefects;
+    }
+
+    public String getmNeedleMark() {
+        return mNeedleMark;
+    }
+
+    public void setmNeedleMark(String mNeedleMark) {
+        this.mNeedleMark = mNeedleMark;
+    }
+
+    public String getmOpenSeam() {
+        return mOpenSeam;
+    }
+
+    public void setmOpenSeam(String mOpenSeam) {
+        this.mOpenSeam = mOpenSeam;
+    }
+
+    public String getmPleats() {
+        return mPleats;
+    }
+
+    public void setmPleats(String mPleats) {
+        this.mPleats = mPleats;
+    }
+
+    public String getmMissingStitches() {
+        return mMissingStitches;
+    }
+
+    public void setmMissingStitches(String mMissingStitches) {
+        this.mMissingStitches = mMissingStitches;
+    }
+
+    public String getmSkipRunOff() {
+        return mSkipRunOff;
+    }
+
+    public void setmSkipRunOff(String mSkipRunOff) {
+        this.mSkipRunOff = mSkipRunOff;
+    }
+
+    public String getmIncorrectLabel() {
+        return mIncorrectLabel;
+    }
+
+    public void setmIncorrectLabel(String mIncorrectLabel) {
+        this.mIncorrectLabel = mIncorrectLabel;
+    }
+
+    public String getmWrongPlacement() {
+        return mWrongPlacement;
+    }
+
+    public void setmWrongPlacement(String mWrongPlacement) {
+        this.mWrongPlacement = mWrongPlacement;
+    }
+
+    public String getmLooseNess() {
+        return mLooseNess;
+    }
+
+    public void setmLooseNess(String mLooseNess) {
+        this.mLooseNess = mLooseNess;
+    }
+
+    public String getmCutDamage() {
+        return mCutDamage;
+    }
+
+    public void setmCutDamage(String mCutDamage) {
+        this.mCutDamage = mCutDamage;
+    }
+
+    public String getmOthers() {
+        return mOthers;
+    }
+
+    public void setmOthers(String mOthers) {
+        this.mOthers = mOthers;
+    }
+
+    public String getmStain() {
+        return mStain;
+    }
+
+    public void setmStain(String mStain) {
+        this.mStain = mStain;
+    }
+
+    public String getmOilMark() {
+        return mOilMark;
+    }
+
+    public void setmOilMark(String mOilMark) {
+        this.mOilMark = mOilMark;
+    }
+
+    public String getmStickers() {
+        return mStickers;
+    }
+
+    public void setmStickers(String mStickers) {
+        this.mStickers = mStickers;
+    }
+
+    public String getmUncutThread() {
+        return mUncutThread;
+    }
+
+    public void setmUncutThread(String mUncutThread) {
+        this.mUncutThread = mUncutThread;
+    }
+
+    public String getmOutOfSpec() {
+        return mOutOfSpec;
+    }
+
+    public void setmOutOfSpec(String mOutOfSpec) {
+        this.mOutOfSpec = mOutOfSpec;
+    }
+
+    public String getmTotalDefect() {
+        return mTotalDefect;
+    }
+
+    public void setmTotalDefect(String mTotalDefect) {
+        this.mTotalDefect = mTotalDefect;
+    }
+
+    public String getmQualityOut() {
+        return mQualityOut;
+    }
+
+    public void setmQualityOut(String mQualityOut) {
+        this.mQualityOut = mQualityOut;
+    }
+
+    public String getmProductionOut() {
+        return mProductionOut;
+    }
+
+    public void setmProductionOut(String mProductionOut) {
+        this.mProductionOut = mProductionOut;
+    }
+
+    public String getmDamage() {
+        return mDamage;
+    }
+
+    public void setmDamage(String mDamage) {
+        this.mDamage = mDamage;
+    }
+
+    public String getmDirty() {
+        return mDirty;
+    }
+
+    public void setmDirty(String mDirty) {
+        this.mDirty = mDirty;
+    }
+
+    public String getmIron() {
+        return mIron;
+    }
+
+    public void setmIron(String mIron) {
+        this.mIron = mIron;
+    }
+
+    public Button getmNext() {
+        return mNext;
+    }
+
+    public void setmNext(Button mNext) {
+        this.mNext = mNext;
+    }
+
+    public Button getmDone() {
+        return mDone;
+    }
+
+    public void setmDone(Button mDone) {
+        this.mDone = mDone;
+    }
+}
