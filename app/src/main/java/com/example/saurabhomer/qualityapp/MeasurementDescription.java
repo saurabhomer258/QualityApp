1408a91cd3d53541effbec22f3f6b53562c7fc65
Anjali Kumari <anjali.kumari@cura.tech>
2020-03-09 13:29:03 +0530

    id fetch

--- app/src/main/java/com/example/saurabhomer/qualityapp/MeasurementDescription.java
+++ app/src/main/java/com/example/saurabhomer/qualityapp/MeasurementDescription.java
@@ -2,6 +2,9 @@ package com.example.saurabhomer.qualityapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MeasurementDescription extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MeasurementDescription extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_measurement_description);
+
+        View view_finishingworking = findViewById(R.id.edt_finishing_working_hour);
+        final EditText editText_tolerance_plus = view_finishingworking.findViewById(R.id.atvCommon);
+
+        View view_measurementreport = findViewById(R.id.edt_measurement_report);
+        final EditText editText_measurementreport = view_measurementreport.findViewById(R.id.atvCommon);
+
+        View view_measurementdescription = findViewById(R.id.edt_measurement_description);
+        final EditText editText_measurementdescription = view_measurementdescription.findViewById(R.id.atvCommon);
+
+        View view_tolerance = findViewById(R.id.edt_tolerance);
+        final EditText editText_tolerance = view_tolerance.findViewById(R.id.atvCommon);
+
+        View view_next= findViewById(R.id.btn_next_m);
+        final Button btn_next = view_next.findViewById(R.id.btnNext);
+
+        View view_done = findViewById(R.id.btn_done_m);
+        final Button btn_done = view_done.findViewById(R.id.btnNext);
     }
 }
--- app/src/main/java/com/example/saurabhomer/qualityapp/Measurment.java
+++ app/src/main/java/com/example/saurabhomer/qualityapp/Measurment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -15,5 +16,15 @@ public class Measurment extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_measurment_description);
+
+        View  measurment= findViewById(R.id.edt_measurment);
+        final EditText editText_measurement = measurment.findViewById(R.id.atvCommon);
+
+        View  tolerance_plus= findViewById(R.id.edt_tolerance_plus);
+        final EditText editText_tolerance_plus = tolerance_plus.findViewById(R.id.atvCommon);
+
+        View  tolerance_minus= findViewById(R.id.edt_tolerance_minus);
+        final EditText editText_tolerance_minus = tolerance_minus.findViewById(R.id.atvCommon);
+
     }
 }
