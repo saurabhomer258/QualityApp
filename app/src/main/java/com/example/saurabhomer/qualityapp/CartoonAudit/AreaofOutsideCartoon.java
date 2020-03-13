62141a7bfb78181f31894eccc5fc0400593639b3
username <anjali.kumari@cura.tech>
2020-03-13 13:53:53 +0530

    ---

---
+++ app/src/main/java/com/example/saurabhomer/qualityapp/CartoonAudit/AreaofOutsideCartoon.java
@@ -0,0 +1,15 @@
+package com.example.saurabhomer.qualityapp.CartoonAudit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.saurabhomer.qualityapp.R;
+
+public class AreaofOutsideCartoon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_areaof_outside_audit);
+    }
+}
---
+++ app/src/main/java/com/example/saurabhomer/qualityapp/InlinePrelineFinal/InlinePrelineFinal.java
@@ -0,0 +1,16 @@
+package com.example.saurabhomer.qualityapp.InlinePrelineFinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.saurabhomer.qualityapp.R;
+
+public class InlinePrelineFinal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inline_preline_final);
+    }
+}
