370923424d0f365d8e7eb55891b9e58f47716282
username <anjali.kumari@cura.tech>
2020-03-18 23:07:26 +0530

    cartoon page complete

--- app/src/main/java/com/example/saurabhomer/qualityapp/CartoonAudit/AreaOfPackingMaterial.java
+++ app/src/main/java/com/example/saurabhomer/qualityapp/CartoonAudit/AreaOfPackingMaterial.java
@@ -1,7 +1,12 @@
 package com.example.saurabhomer.qualityapp.CartoonAudit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import com.example.saurabhomer.qualityapp.R;
 
@@ -11,5 +16,71 @@ public class AreaOfPackingMaterial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_area_of_packing_material);
+
+        View view_edt_hour = findViewById(R.id.edt_hour);
+        final EditText editText_edt_hour = view_edt_hour.findViewById(R.id.atvCommon);
+
+        View view_edt_cartoon = findViewById(R.id.edt_cartoon);
+        final EditText editText_edt_cartoon = view_edt_cartoon.findViewById(R.id.atvCommon);
+
+
+        View view_edt_check_cartoon = findViewById(R.id.edt_check_cartoon);
+        final EditText editText_edt_check_cartoon = view_edt_check_cartoon.findViewById(R.id.atvCommon);
+
+
+        View view_edt_packinglabel = findViewById(R.id.edt_packinglabel);
+        final RadioButton radioButton_view_edt_packinglabel = view_edt_packinglabel.findViewById(R.id.ok);
+
+
+        View view_edt_additionallabel = findViewById(R.id.edt_additionallabel);
+        final RadioButton radioButton_edt_additionallabel = view_edt_additionallabel.findViewById(R.id.ok);
+
+        View view_edt_misplace = findViewById(R.id.edt_misplace);
+        final RadioButton radioButton_edt_misplace = view_edt_misplace.findViewById(R.id.ok);
+
+        View view_edt_incorrectlabel = findViewById(R.id.edt_incorrectlabel);
+        final RadioButton radioButton_edt_incorrectlabel= view_edt_incorrectlabel.findViewById(R.id.ok);
+
+        View view_edt_damagelabel = findViewById(R.id.edt_damagelabel);
+        final RadioButton radioButton_edt_damagelabel= view_edt_damagelabel.findViewById(R.id.ok);
+
+
+        View view_edt_incorrectUPC = findViewById(R.id.edt_incorrectUPC);
+        final RadioButton radioButton_edt_incorrectUPC= view_edt_incorrectUPC.findViewById(R.id.ok);
+
+        View view_edt_incorrectSize = findViewById(R.id.edt_incorrectSize);
+        final RadioButton radioButton_edt_incorrectSize= view_edt_incorrectSize.findViewById(R.id.ok);
+
+        View view_edt_incorrectHanger = findViewById(R.id.edt_incorrectHanger);
+        final RadioButton radioButton_edt_incorrectHanger= view_edt_incorrectHanger.findViewById(R.id.ok);
+
+        View view_edt_polywarning = findViewById(R.id.edt_polywarning);
+        final RadioButton radioButton_edt_polywarning= view_edt_polywarning.findViewById(R.id.ok);
+
+
+        View view_edt_total_defect_count = findViewById(R.id.edt_total_defect_count);
+        final EditText editText_edt_total_defect_count = view_edt_total_defect_count.findViewById(R.id.atvCommon);
+
+
+        View btn_next = findViewById(R.id.btn_next);
+        Button bt_next = btn_next.findViewById(R.id.btnNext);
+
+        View btn_done = findViewById(R.id.btn_done_measurement);
+        Button bt_done = btn_done.findViewById(R.id.btnNext);
+
+        bt_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(AreaOfPackingMaterial.this, AreaOfPackingMaterial.class);
+                startActivity(i);
+            }
+        });
+
+        bt_done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/saurabhomer/qualityapp/CartoonAudit/AreaofOutsideCartoon.java
+++ app/src/main/java/com/example/saurabhomer/qualityapp/CartoonAudit/AreaofOutsideCartoon.java
@@ -1,7 +1,13 @@
 package com.example.saurabhomer.qualityapp.CartoonAudit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.example.saurabhomer.qualityapp.R;
 
@@ -11,5 +17,54 @@ public class AreaofOutsideCartoon extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_areaof_outside_audit);
+
+        View view_edt_hour = findViewById(R.id.edt_hour);
+        final EditText editText_edt_hour = view_edt_hour.findViewById(R.id.atvCommon);
+
+        View view_edt_cartoon = findViewById(R.id.edt_cartoon);
+        final EditText editText_edt_cartoon = view_edt_cartoon.findViewById(R.id.atvCommon);
+
+        View view_edt_cartoon_shiping = findViewById(R.id.edt_cartoon_shiping);
+        final RadioButton radioButton_edt_cartoon_shiping = view_edt_cartoon_shiping.findViewById(R.id.ok);
+
+        View view_edt_printing= findViewById(R.id.edt_printing);
+        final RadioButton radioButton_edt_printing = view_edt_printing.findViewById(R.id.ok);
+
+        View view_edt_car_size = findViewById(R.id.edt_car_size);
+        final RadioButton radioButton_edt_car_size = view_edt_car_size.findViewById(R.id.ok);
+
+        View view_edt_cartoon_no = findViewById(R.id.edt_cartoon_no);
+        final RadioButton radioButton_edt_cartoon_no = view_edt_cartoon_no.findViewById(R.id.ok);
+
+        View view_edt_barcode = findViewById(R.id.edt_barcode);
+        final RadioButton radioButton_edt_barcode = view_edt_barcode.findViewById(R.id.ok);
+
+        View view_edt_car_fly = findViewById(R.id.edt_car_fly);
+        final RadioButton radioButton_edt_car_fly= view_edt_car_fly.findViewById(R.id.ok);
+
+
+        final TextView textView_edt_hour8 = (TextView) findViewById(R.id.remarks);
+
+        View btn_next = findViewById(R.id.btn_next);
+        Button bt_next = btn_next.findViewById(R.id.btnNext);
+
+        View btn_done = findViewById(R.id.btn_done_measurement);
+        Button bt_done = btn_done.findViewById(R.id.btnNext);
+
+        bt_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(AreaofOutsideCartoon.this, AreaofOutsideCartoon.class);
+                startActivity(i);
+            }
+        });
+
+        bt_done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(AreaofOutsideCartoon.this, AreaOfInsideCartoon.class);
+                startActivity(i);
+            }
+        });
     }
 }
