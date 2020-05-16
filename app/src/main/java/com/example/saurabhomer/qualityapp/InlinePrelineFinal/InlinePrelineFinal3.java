9b778806ae934972d58368935040507bce542b76
saurabhomer258 <saurabh@bobble.ai>
2020-05-16 14:01:16 +0530

    ..

--- app/src/main/java/com/example/saurabhomer/qualityapp/InlinePrelineFinal/InlinePrelineFinal3.java
+++ app/src/main/java/com/example/saurabhomer/qualityapp/InlinePrelineFinal/InlinePrelineFinal3.java
@@ -32,6 +32,7 @@ public class InlinePrelineFinal3 extends AppCompatActivity {
 
     static ArrayList<InlinePrelineFinalModel1> inlinePrelineFinalModel1List = new ArrayList<>();
     private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,39 +43,55 @@ public class InlinePrelineFinal3 extends AppCompatActivity {
         final EditText edt_critical = findViewById(R.id.edt_critical).findViewById(R.id.atvCommon);
         final EditText edt_minor = findViewById(R.id.edt_minor).findViewById(R.id.atvCommon);
         final EditText edt_major = findViewById(R.id.edt_major).findViewById(R.id.atvCommon);
-        final TextView txt_result = findViewById(R.id.txt_result);
+
         final TextView txt_remark = findViewById(R.id.txt_remark);
         final TextView txt_total = findViewById(R.id.txt_total);
         final TextView txt_aql = findViewById(R.id.txt_aql);
         final TextView txt_inspection = findViewById(R.id.txt_inspection);
         final TextView txt_samplesize = findViewById(R.id.txt_samplesize);
-        Button next =findViewById(R.id.btn_next).findViewById(R.id.btnNext);
-        Button done =findViewById(R.id.btn_done).findViewById(R.id.btnNext);
+        Button next = findViewById(R.id.btn_next).findViewById(R.id.btnNext);
+        Button done = findViewById(R.id.btn_done).findViewById(R.id.btnNext);
+        Button result_btn = findViewById(R.id.btn_getResult).findViewById(R.id.btnNext);
+
         TextView text_critical_acc = findViewById(R.id.txt_critical_Accept);
         TextView text_critical_rej = findViewById(R.id.txt_critical_reject);
         TextView text_major_acc = findViewById(R.id.txt_major_Accept);
         TextView text_major_rej = findViewById(R.id.txt_major_reject);
         TextView text_minor_acc = findViewById(R.id.txt_minor_Accept);
         TextView text_minor_rej = findViewById(R.id.txt_minor_reject);
-        InlinePreLineHandler inlinePreLineHandler = new InlinePreLineHandler(inlinePrelineFinalModel.getSelect_level(),inlinePrelineFinalModel.getInspection_level(),Integer.parseInt(inlinePrelineFinalModel.getQuantity()));
-        ResultModel resultModel = inlinePreLineHandler.getResult();
+        InlinePreLineHandler inlinePreLineHandler = new InlinePreLineHandler(inlinePrelineFinalModel.getSelect_level(), inlinePrelineFinalModel.getInspection_level(), Integer.parseInt(inlinePrelineFinalModel.getQuantity()));
+        final ResultModel resultModel = inlinePreLineHandler.getResult();
 
         String select_1 = inlinePrelineFinalModel.getSelect_level();
         String inspection_1 = inlinePrelineFinalModel.getInspection_level();
         txt_aql.setText(select_1);
         txt_inspection.setText(inspection_1);
         txt_samplesize.setText(resultModel.getSAMPLESIZE());
-        text_critical_acc.setText(resultModel.getCRITICAL_ACE()+"");
-        text_major_acc.setText(resultModel.getCRITICAL_ACE()+"");
-        text_minor_acc.setText(resultModel.getCRITICAL_ACE()+"");
-        text_critical_rej.setText(resultModel.getCRITICAL_REJ()+"");
-        text_major_rej.setText(resultModel.getCRITICAL_REJ()+"");
-        text_minor_rej.setText(resultModel.getCRITICAL_REJ()+"");
+        text_critical_acc.setText(resultModel.getCRITICAL_ACE() + "");
+        text_major_acc.setText(resultModel.getCRITICAL_ACE() + "");
+        text_minor_acc.setText(resultModel.getCRITICAL_ACE() + "");
+        text_critical_rej.setText(resultModel.getCRITICAL_REJ() + "");
+        text_major_rej.setText(resultModel.getCRITICAL_REJ() + "");
+        text_minor_rej.setText(resultModel.getCRITICAL_REJ() + "");
 //        String minor = edt_minor.getText().toString();
 //        String major = edt_major.getText().toString();
 //        int total = Integer.parseInt(minor)+Integer.parseInt(major);
 //        txt_total.setText(total);
-
+        result_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(edt_major.getText().toString());
+                int b = Integer.parseInt(edt_critical.getText().toString());
+                int c = Integer.parseInt(edt_minor.getText().toString());
+                int sum = (a + b + c);
+                txt_total.setText("TOTAL    " + sum);
+                if (sum <= resultModel.getCRITICAL_ACE()) {
+                    txt_remark.setText("REMARK   PASS");
+                } else {
+                    txt_remark.setText("REMARK    FAIL");
+                }
+            }
+        });
         next.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -87,9 +104,19 @@ public class InlinePrelineFinal3 extends AppCompatActivity {
                 inlinePrelineFinalModel1.setMajor(edt_major.getText().toString());
                 inlinePrelineFinalModel1.setMinor(edt_minor.getText().toString());
                 inlinePrelineFinalModel1.setCritical(edt_critical.getText().toString());
-                inlinePrelineFinalModel1.setRemark(txt_remark.getText().toString());
-                inlinePrelineFinalModel1.setResult(txt_result.getText().toString());
-                inlinePrelineFinalModel1.setTotal(txt_total.getText().toString());
+
+//                inlinePrelineFinalModel1.setResult(txt_result.getText().toString());
+                int a = Integer.parseInt(edt_major.getText().toString());
+                int b = Integer.parseInt(edt_critical.getText().toString());
+                int c = Integer.parseInt(edt_minor.getText().toString());
+                int sum = (a + b + c);
+                inlinePrelineFinalModel1.setTotal(sum + "");
+                if (sum <= resultModel.getCRITICAL_ACE()) {
+                    inlinePrelineFinalModel1.setRemark("PASS");
+
+                } else {
+                    inlinePrelineFinalModel1.setRemark("FAIL");
+                }
                 inlinePrelineFinalModel1List.add(inlinePrelineFinalModel1);
                 Intent i = new Intent(InlinePrelineFinal3.this, InlinePrelineFinal3.class);
                 startActivity(i);
@@ -108,8 +135,18 @@ public class InlinePrelineFinal3 extends AppCompatActivity {
                 inlinePrelineFinalModel1.setMajor(edt_major.getText().toString());
                 inlinePrelineFinalModel1.setMinor(edt_minor.getText().toString());
                 inlinePrelineFinalModel1.setCritical(edt_critical.getText().toString());
-                inlinePrelineFinalModel1.setRemark(txt_remark.getText().toString());
-                inlinePrelineFinalModel1.setResult(txt_result.getText().toString());
+                // inlinePrelineFinalModel1.setResult(txt_result.getText().toString());
+                int a = Integer.parseInt(edt_major.getText().toString());
+                int b = Integer.parseInt(edt_critical.getText().toString());
+                int c = Integer.parseInt(edt_minor.getText().toString());
+                int sum = (a + b + c);
+                if (sum <= resultModel.getCRITICAL_ACE()) {
+                    inlinePrelineFinalModel1.setRemark("PASS");
+
+                } else {
+                    inlinePrelineFinalModel1.setRemark("FAIL");
+                }
+                inlinePrelineFinalModel1.setTotal(sum + "");
                 inlinePrelineFinalModel1List.add(inlinePrelineFinalModel1);
                 inlinePrelineFinalModel.setInlinePrelineFinalModel1(inlinePrelineFinalModel1List);
                 FirebaseDatabase.getInstance().getReference("inlinepreline").child(STYLE_NUMBER).setValue(inlinePrelineFinalModel).addOnCompleteListener(new OnCompleteListener<Void>() {
